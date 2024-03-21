b5485d39fe884bd5bd4f69740ec6f44a2357eb6f
Marcus Burghardt <maburgha@redhat.com>
2024-03-21 12:37:55 +0100

    Minor improvements in custom script

--- files/vscode_settings.py
+++ files/vscode_settings.py
@@ -1,9 +1,6 @@
-# This is a very simple script created very fast with only
-# goal to deal with VSCode json files.
+# This is a very simple script created to deal with VSCode JSON files.
 # Parameters are not validated, as they are hardcoded in this Ansible Module.
 # So, be careful if using it manually or if modifying the Module.
-# 
-# And, feel free to suggest improvements. : )
 
 #!/usr/bin/python
 import json
@@ -16,39 +13,37 @@ if len(sys.argv) == 6:
     parameter = sys.argv[4]
     value = sys.argv[5]
 else:
-   print ("Syntax: ",sys.argv[0]," <add|remove> <file> <section> <parameter> <value>")
-   print ("Syntax: ",sys.argv[0]," add /tmp/workstaces.json folders path /home/user/DEV/BUG000_TTT")
-   sys.exit(1)
+    print("Syntax: ", sys.argv[0], " <add|remove> <file> <section> <parameter> <value>")
+    print("Syntax: ", sys.argv[0], " add /tmp/workstaces.json folders path /home/user/DEV/BUG000_TTT")
+    sys.exit(1)
 
 with open(json_file, "r+") as roFile:
     data = json.load(roFile)
     roFile.close
 
-# Prepare the entry in a generic way
-entry = { parameter: value }
+entry = {parameter: value}
 
 if action == 'add':
     try:
         if entry not in data[section]:
             data[section].append(entry)
-    except:
-        print ("Cannot add the entry. Possibly some argument is wrong.")
+    except SyntaxError:
+        print("Cannot add the entry. Possibly some argument is wrong.")
         # I know. This script was made to work with this Ansible role.
-        sys.exit(0)
+        sys.exit(1)
 elif action == 'remove':
     try:
         data[section].remove(entry)
-    except:
-        print ("Cannot remove the entry. Possibly some argument is wrong or the entry doesn't exist.")
-        sys.exit(0)
+    except SyntaxError:
+        print("Cannot remove the entry. Possibly some argument is wrong or the entry doesn't exist.")
+        sys.exit(1)
 else:
-    print ("Invalid Action")
+    print("Invalid Action")
     sys.exit(2)
 
-#print(json.dumps(data, indent = 8, sort_keys=True))
 with open(json_file, "w") as rwFile:
-    rwFile.seek(0)  # rewind
-    rwFile.write(json.dumps(data, indent = 8, sort_keys=True))
+    rwFile.seek(0)
+    rwFile.write(json.dumps(data, indent=8, sort_keys=True))
     rwFile.truncate()
     rwFile.close
 sys.exit(0)
